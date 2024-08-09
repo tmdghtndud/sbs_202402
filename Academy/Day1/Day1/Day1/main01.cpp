c7ca76eea96998eb62570c396f5365c7bedc850c
sbscerti <tmdghtndud@gmail.com>
2024-08-09 21:12:56 +0900

    [수영] 작업올림

---
+++ Academy/Day1/Day1/Day1/main01.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void main()
+{
+	printf("동해물과 백두산이\n");
+	printf("마르고 닳도록\n");
+	printf("하나님이 보우하사\n");
+	printf("우리나라 만세\n");
+	// 줄을 띄우려면 \n은 엔터(줄띄움)의 효과를 가지고있다.
+
+	// 실습
+	// 애국가 1절 출력하기 (줄띄움 포함하기!)
+	// 
+	// 동해물과 백두산이
+	// ...
+
+	// printf("[하고싶은말]"); 
+	// printf("");  << 이형태를 꼭 유지해야합니다.
+}
+
+//ctrl + f5 로 실행
+
+// 기본적으로 프로그래밍은
+// 항상 (어떤언어든, 어떤 프레임워크든 동일합니다.)
+// main 함수를 실행합니다.
+
+// main 함수는 프로그래밍 시작점
+// #include <iostream>
+//   iostream << [input output] stream 을 다루는 내용을
+//					포함한다.
+// using namespace std;
+// 얘네는 무엇인가?
+// using namespace std; << 얘는 아직 안쓰고있어요. 사실 지워도 무방합니다.
